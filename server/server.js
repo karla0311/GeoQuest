c2a7ea74d5e1a52dda5646b445c4a21b776be440
Aditya Rao <aditya.k.rao@sjsu.edu>
2026-03-20 01:47:49 -0700

    fix(server): load .env from server/ dir to match README

--- server/server.js
+++ server/server.js
@@ -1,10 +1,4 @@
-import dotenv from "dotenv";
-import { resolve, dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-dotenv.config({ path: resolve(__dirname, "../.env") });
-
+import "dotenv/config";
 import app from "./app.js";
 
 const PORT = process.env.PORT || 8000;
